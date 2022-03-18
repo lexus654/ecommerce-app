17151cc2acf9ede23b59ec936a349abb75700061
lexus654 <ronald.laz@tup.edu.ph>
2022-03-18 11:21:17 +0800

    push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Starting Page</div>
+    </div>
+  );
+}
+
+export default App;
