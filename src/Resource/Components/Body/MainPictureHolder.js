1cad942905fd869e32dd242a2f33eb22d06c624d
lexus654 <ronald.laz@tup.edu.ph>
2022-03-28 15:54:23 +0800

    styling need to be done iusing styled components

--- src/Resource/Components/Body/MainPictureHolder.js
+++ src/Resource/Components/Body/MainPictureHolder.js
@@ -1,10 +1,13 @@
+import mainPicture from "../../images/image-product-1-thumbnail.jpg";
+
+// Styling compones
 function MainPictureHolder() {
   return (
-    // <img src="" alt="">
-    //   {" "}
-
-    // </img>
-    <h1>Hello</h1>
+    <img
+      className="mainProduct picture"
+      src={mainPicture}
+      alt="Main product"
+    ></img>
   );
 }
 
