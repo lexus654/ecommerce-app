8a05f0cd0225698904b6902e14f2ee5805530d1b
lexus654 <ronald.laz@tup.edu.ph>
2022-03-29 10:43:51 +0800

    done using styled components

--- src/Resource/Components/Body/GridPictures.js
+++ src/Resource/Components/Body/GridPictures.js
@@ -2,21 +2,22 @@ import gridPicture1 from "../../images/image-product-1.jpg";
 import gridPicture2 from "../../images/image-product-2.jpg";
 import gridPicture3 from "../../images/image-product-3.jpg";
 import gridPicture4 from "../../images/image-product-4.jpg";
+import styled from "styled-components";
+
+const Holder = styled.ul`
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 function GridPictures() {
   return (
-    <ul
-      style={{
-        width: "300px",
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-    >
+    <Holder>
       <img style={{ width: "60px" }} src={gridPicture1} alt="1st"></img>
       <img style={{ width: "60px" }} src={gridPicture2} alt="2nd"></img>
       <img style={{ width: "60px" }} src={gridPicture3} alt="3rd"></img>
       <img style={{ width: "60px" }} src={gridPicture4} alt="4th"></img>
-    </ul>
+    </Holder>
   );
 }
 
