eaa1930d04e581bdd9040d3f8a8e96e6e282f761
lexus654 <ronald.laz@tup.edu.ph>
2022-03-31 22:54:06 +0800

    Refactored Header css

--- src/Resource/Components/Header.js
+++ src/Resource/Components/Header.js
@@ -1,11 +1,47 @@
 import logo from "../images/logo.svg";
 import cart from "../images/icon-cart.svg";
 import userPhoto from "../images/image-avatar.png";
-import "./Header.css";
+// import "./Header.css";
+import styled from "styled-components";
+
+const Holder = styled.div`
+  height: 100px;
+  background-color: aqua;
+  padding: 0px 60px;
+  &,
+  ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  & .logo {
+    width: 150px;
+  }
+  & li {
+    font-size: 0.9rem;
+    position: relative;
+    right: 250px;
+    color: gray;
+    cursor: pointer;
+    margin: 0 10px;
+    list-style-type: none;
+  }
+  & .cart,
+  .userPhoto {
+    height: 35px;
+    width: 35px;
+  }
+
+  & .list-end {
+    display: flex;
+    width: 100px;
+    justify-content: space-between;
+  }
+`;
 
 function Header() {
   return (
-    <div className="container">
+    <Holder>
       <img className="logo" src={logo} alt="logo"></img>
       <ul className="list-centered">
         <li>Collections</li>
@@ -18,7 +54,7 @@ function Header() {
         <img className="cart" src={cart} alt="cart"></img>
         <img className="userPhoto" src={userPhoto} alt="user"></img>
       </ul>
-    </div>
+    </Holder>
   );
 }
 
