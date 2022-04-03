240f6c10abdd0e159ff7678450f90a8b2be8718f
lexus654 <ronald.laz@tup.edu.ph>
2022-04-03 18:37:27 +0800

    style of main text done

--- src/Resource/Components/Body/TextHolder.js
+++ src/Resource/Components/Body/TextHolder.js
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 const Holder = styled.div`
-  text-align: left;
   width: 75%;
-  background-color: coral;
-  & span {
-    color: blue;
-    font-size: 0.9rem;
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: left;
+  margin-bottom: 20px;
   & h1 {
-    font-size: 2.7rem;
+    font-family: "Kumbh Sans", sans-serif;
+    font-size: 2.3rem;
+    line-height: 110%;
+    margin-bottom: 20px;
+  }
+  & span {
+    color: hsl(26, 100%, 55%);
+    font-family: "Kumbh Sans", sans-serif;
+    font-size: 0.8rem;
+    font-weight: 700;
+    letter-spacing: 3px;
+    margin-bottom: 20px;
   }
   & p {
     font-size: 0.9rem;
+    font-weight: 400;
+    font-family: "Kumbh Sans", sans-serif;
+    color: hsl(219, 9%, 45%);
   }
 `;
 
@@ -19,7 +32,7 @@ function TextHolder() {
   return (
     <Holder>
       <span>SNEAKER COMPANY</span>
-      <h1>Fall Limited Sneakers</h1>
+      <h1>Fall Limited Edition Sneakers</h1>
       <p>
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they'll withstand everything the
