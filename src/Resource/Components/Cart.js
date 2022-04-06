50fb5cd743328a8465d2b7c968fe43a2ca264553
lexus654 <ronald.laz@tup.edu.ph>
2022-04-06 21:49:43 +0800

    added the cart menu

---
+++ src/Resource/Components/Cart.js
@@ -0,0 +1,16 @@
+function Cart() {
+  return (
+    <div>
+      <p>Name</p>
+      <div>
+        <p>title</p>
+        <img>img</img>
+        <p>price and quantity bill</p>
+      </div>
+      <button>delete</button>
+      <button>Check out</button>
+    </div>
+  );
+}
+
+export default Cart;
