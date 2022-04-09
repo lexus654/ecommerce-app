8d4077bf9fe391e1f1d583bd823ee25d1e40779e
lexus654 <ronald.laz@tup.edu.ph>
2022-04-09 19:29:31 +0800

    magulo pa din ang cart

--- src/Resource/Components/Cart.js
+++ src/Resource/Components/Cart.js
@@ -4,31 +4,40 @@ import deletePic from "../images/icon-delete.svg";
 
 const HolderAbove = styled.div`
   width: 250px;
-  background-color: lightblue;
+  border: 1px solid black;
   position: absolute;
   top: 100px;
   right: 100px;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: space-between;
+  padding: 10px 0;
+
   & .title {
     font-size: 0.8rem;
     text-align: left;
     margin: 10px;
     font-weight: bolder;
+    margin-bottom: 10px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid black;
   }
   & .product-name,
   .cart-price {
     font-size: 0.7rem;
     text-align: left;
+    margin-right: 15px;
   }
   & .cart-picture {
     width: 40px;
+    margin-right: 15px;
   }
   & .product-holder {
     display: flex;
     flex-direction: row;
     justify-content: center;
+    margin-bottom: 20px;
   }
   & .product-holder-column {
     display: flex;
