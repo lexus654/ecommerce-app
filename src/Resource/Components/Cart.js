c941dcb1f6cd4eb4ca66c3d110ca62ce62f77c7c
lexus654 <ronald.laz@tup.edu.ph>
2022-04-09 19:22:06 +0800

    spaghetti code

--- src/Resource/Components/Cart.js
+++ src/Resource/Components/Cart.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Content from "./Content";
+import deletePic from "../images/icon-delete.svg";
 
 const HolderAbove = styled.div`
   width: 250px;
@@ -16,8 +17,40 @@ const HolderAbove = styled.div`
     margin: 10px;
     font-weight: bolder;
   }
-  & .product-name {
+  & .product-name,
+  .cart-price {
     font-size: 0.7rem;
+    text-align: left;
+  }
+  & .cart-picture {
+    width: 40px;
+  }
+  & .product-holder {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  & .product-holder-column {
+    display: flex;
+    flex-direction: column;
+  }
+  & .delete {
+    width: 19px;
+    height: 19px;
+  }
+
+  & .checkout {
+    background-color: hsl(26, 100%, 55%);
+    border: none;
+    border-radius: 10px;
+    width: 100%;
+    height: 40px;
+    color: hsl(223, 64%, 98%);
+    font-weight: bold;
+    letter-spacing: 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
@@ -27,14 +60,20 @@ function Cart(props) {
     <HolderAbove>
       <p className="title">Cart</p>
       <div className="product-holder">
-        <p className="product-name">{props.productName}</p>
-        {/* <img src={props.mainPicture}></img> */}
-        <p>
-          {props.price} x {amount} {props.price * amount}
-        </p>
-        <button>delete</button>
+        <img
+          className="cart-picture"
+          src={props.mainPicture}
+          alt="pic cart"
+        ></img>
+        <div className="product-holder-column">
+          <p className="product-name">{props.productName}</p>
+          <p className="cart-price">
+            {props.price} x {amount} {props.price * amount}
+          </p>
+        </div>
+        <img className="delete" src={deletePic} alt="delerte"></img>
       </div>
-      <button>Check out</button>
+      <button className="checkout">Check out</button>
     </HolderAbove>
   );
 }
